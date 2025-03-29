6ef3bd9dc938ee28a1d2d44b43ba388042c0a470
Kolmas225 <169401425+Kolmas225@users.noreply.github.com>
2025-03-30 01:27:43 +0800

    emacs: add ace-window-prefix
    taken from karthinks' blog

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1112,6 +1112,7 @@ Used to preselect nearest headings and imenu items.")
 
 (use-package ace-window
   :custom
+  (aw-dispatch-always t)
   (aw-scope 'frame)
   (aw-keys '(?n ?e ?i ?a ?t ?s ?r ?c))
   (ace-window-display-mode t)	     ;display dispatch char at modeline
@@ -1119,6 +1120,7 @@ Used to preselect nearest headings and imenu items.")
   (aw-leading-char-face ((nil (:foreground unspecified :inherit avy-lead-face))))
   :bind
   ("M-o" . #'ace-window)
+  ("M-O" . #'ace-window-prefix)
   :config
 
   (defun my/ace-window-split-frame-if-single-window (&rest _)
@@ -1129,6 +1131,24 @@ Used to preselect nearest headings and imenu items.")
   
   (advice-add #'aw--switch-buffer :override
 	          (lambda () (consult-buffer)))
+
+  (defun ace-window-prefix ()
+    "Use `ace-window' to display the buffer of the next command.
+The next buffer is the buffer displayed by the next command invoked
+immediately after this command (ignoring reading from the minibuffer).
+Creates a new window before displaying the buffer.
+When `switch-to-buffer-obey-display-actions' is non-nil,
+`switch-to-buffer' commands are also supported."
+    (interactive)
+    (display-buffer-override-next-command
+     (lambda (buffer _)
+       (let (window type)
+         (setq
+          window (aw-select (propertize " ACE" 'face 'mode-line-highlight))
+          type 'reuse)
+         (cons window type)))
+     nil "[ace-window]")
+    (message "Use `ace-window' to display next command buffer..."))
   
   (setq aw-dispatch-alist
 	    '((?m aw-swap-window "Swap Window")
