d5e692576b59f9284e0d95b0bed5327f0a65d182
Kolmas225 <169401425+Kolmas225@users.noreply.github.com>
2025-06-03 17:06:50 +0800

    emacs: added .dir-locals.el

---
+++ emacs/.config/emacs/.dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((flycheck-disabled-checkers . (emacs-lisp emacs-lisp-checkdoc)))))
